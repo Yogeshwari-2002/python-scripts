6ec4ed0df4ce8f6625491e437d8fa393f70ea62a
Yogeshwari <yogeshwarin123@gmail.com>
2024-05-05 19:51:40 +0530

    This is my first script for strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+name = "Yogeshwari"
+print("Hello , " + name)
+print(len(name))
+print(name[3])
+print(name[0:5]) #including 0 but not 5
+print(name[:-3]) # getting substracted from len of string and displayed the characters from 0th index
+print(name[-4:len(name) -2])
+print("lets use for loop")
+for character in name:
+  print(character)
+
+#Multiline strings
+Moments = ''' Hello I am Yogeshwari....
+\n I am a younger daughter of my family . \n I want to share my beautiful moments when a undesirable thought come into my life , A guy with pure soul whos name is Hemant . I am very happy to meet him.
+\n I feel there is something special about him that day when firstly he talk to me on 23rd of april.
+\n After we started to share personal and each little things that encontering in his and mine life. That day never goes out of from  my beautiful and sweet memories ever.
+\n Getting proposal of him to me it was jusss like surviving a bestest day in my life . I started to paint my thoughts with lots of love and smiles with him . I started to live a dreamy life from that day only .
+\n I were never known to him , I never see him earlier but may be God has a beautiful plan to get me to him unexpectedly .
+\n The journey starts from there to here going well and I wish we will stay long forever with each other.'''
+print(Moments)
+
